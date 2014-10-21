4df40c68ff1345d98f639ebafafc087dc3ab5a7a
Roger Smith <rogerlsmith@live.com>
2014-10-20 23:50:14 -0400

    server updates

--- add.php
+++ add.php
@@ -1,8 +1,14 @@
 <?php
-        if (isset($_FILES['uploaded_file'])) {
-                $file_path = "upload/";
-                $file_path = $file_path . basename( $_FILES['uploaded_file']['name']);
-            move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path);
-            file_put_contents("log.txt", serialize($_FILES));
-        }
+    $file = fopen ("output.txt", "w");
+    fwrite ($file, "File: ");
+    fwrite ($file, $_FILES['file']['tmp_name'] );
+    fclose ($file);
+
+    if (isset($_FILES['file']['tmp_name'])) {
+        $file_path = "upload/";
+        $file_path = $file_path . basename( $_FILES['file']['tmp_name']);
+        move_uploaded_file($_FILES['file']['tmp_name'], $file_path);
+        file_put_contents("log.txt", serialize($_FILES));
+    }
 ?>
+
---
+++ browse.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>
+concept browser
+</title>
+</head>
+<body>
+browse the files
+</body>
+</html>
