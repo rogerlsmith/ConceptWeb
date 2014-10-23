0c52b0623c49597fca145e762d3092b55c51a76e
Roger Smith <rogerlsmith@live.com>
2014-10-22 23:43:25 -0400

    browser cleanup

--- browse.php
+++ browse.php
@@ -1,26 +1,37 @@
 <html>
 <head>
-<title>
-concept browser
-</title>
+<title> concept browser </title>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 </head>
 <body>
 
 <?php
 
   $files = scandir('upload');
-  sort($files); // this does the sorting
-  foreach($files as $file){
-    if (($file != ".") && ($file != "..")) {
-      echo $file;
+  sort($files);                         // this does the sorting
 ?>
-      <audio controls>
-        <source src="http://rogerlsmith.net/concept/upload/<?= $file; ?>" type="audio/mpeg" >
-      </audio>
-      <br />
+  <div style="display:inline;">
+
+<?php
+      foreach($files as $file) {
+        if (($file != ".") && ($file != "..")) {
+?>
+      <div style="display:inline-block;min-width:250px;overflow:hidden;">
+        <?= $file; ?>
+      </div>
+      <div style="display:inline-block;min-width:250px;overflow:hidden;">
+        <audio controls>
+          <source src="http://rogerlsmith.net/concept/upload/<?= $file; ?>" type="audio/mpeg" >
+        </audio>
+      </div>
 <?php
     }
+?>
+  </div><div style="clear:both;"></div>
+<?php
 }
 ?>
 </body>
 </html>
+
