ee120758ebb0d8d4339509e6a16beba5b58b4d8e
Roger Smith <rogerlsmith@live.com>
2014-10-24 23:30:41 -0400

    new content & added bootsrap

--- browse.php
+++ browse.php
@@ -4,12 +4,12 @@
         <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
         <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
     </head>
-    
     <body>
 
 <?php
-        $files = scandir('upload');
-        sort($files);                         // this does the sorting
+
+    $files = scandir('upload');
+    sort($files);                         // this does the sorting
 ?>
         <div style="display:inline;">
 
@@ -17,21 +17,21 @@
         foreach($files as $file) {
             if (($file != ".") && ($file != "..")) {
 ?>
-                <div style="display:inline-block;min-width:250px;overflow:hidden;">
-                  <?= $file; ?>
-                </div>
-      
-                <div style="display:inline-block;min-width:250px;overflow:hidden;">
-                  <audio controls>
-                    <source src="http://rogerlsmith.net/concept/upload/<?= $file; ?>" type="audio/mpeg" >
-                  </audio>
-                </div>
+        <div style="display:inline-block;min-width:250px;overflow:hidden;">
+            <?= $file; ?>
+        </div>
+            
+        <div style="display:inline-block;min-width:250px;overflow:hidden;">
+            <audio controls>
+              <source src="http://rogerlsmith.net/concept/upload/<?= $file; ?>" type="audio/mpeg" >
+            </audio>
+        </div>
 <?php
-            }
+    }
 ?>
-            </div><div style="clear:both;"></div>
+    </div><div style="clear:both;"></div>
 <?php
-        }
+}
 ?>
     </body>
 </html>
--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 
 <body>
 <h2>Welcome to my new project.</h2>
-<p> To browse uploaded media, click <a href="browse.php">here</a>.</p>
-<p>You will be able to download the Android app very soon from this page.  Stay tuned!</p>
+<p> To browse uploaded media, click <a href="browse.php">here</a>.</p><br/>
+<p>You can now download and install the Android app from this site.  Please click <a href="http://rogerlsmith.net/concept/InstallApp/hybridConcept.apk">here</a> to install the app now.</p><br/>
+<p>Please note that your phone will need to allow apps from third parties to be installed on your phone.  You will need to go to Settings then  Security and turn on Unknown Sources.  This allows you to install apps that are not on the Google Play Store.</p><br/>
+<p>Once the app is downloaded to your phone, click on it to install.</p><br/>
+<p>For a quick help note, click <a href="help.html">here</a>.</p>
 </body>
 </html>
 
