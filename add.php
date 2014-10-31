42478be8138f60668385832dfbaf9335760557db
Roger Smith <rogerlsmith@live.com>
2014-10-30 22:22:05 -0400

    space removal

--- add.php
+++ add.php
@@ -1,12 +1,7 @@
 <?php
-        $file = fopen ("output.txt", "w");
-        fwrite ($file, "File: ");
-        fwrite ($file, $_FILES['file']['tmp_name'] );
-        fclose ($file);
-
-        if (isset($_FILES['file']['tmp_name'])) {
-            $file_path = "upload/";
-            $file_path = $file_path . basename( $_FILES['file']['tmp_name']) .".mp3";
-            move_uploaded_file($_FILES['file']['tmp_name'], $file_path);
-}
+    if (isset($_FILES['file']['tmp_name'])) {
+        $file_path = "upload/";
+        $file_path = $file_path . basename( $_FILES['file']['tmp_name']) .".mp3";
+        move_uploaded_file($_FILES['file']['tmp_name'], $file_path);
+    }
 ?>
