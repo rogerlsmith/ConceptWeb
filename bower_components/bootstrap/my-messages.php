da0c9da6e6154d34cd53d630860193d7e06c6f8b
Roger Smith <rogerlsmith@live.com>
2014-11-18 23:35:16 -0500

    new homepage

--- bower_components/bootstrap/my-messages.php
+++ bower_components/bootstrap/my-messages.php
@@ -3,9 +3,46 @@ http://untame.net/2013/06/how-to-build-a-functional-login-form-with-php-twitter-
 -->
 <?php
     require("config.php");
-    if(empty($_SESSION['user'])) 
+    if(!empty($_SESSION['user'])) 
     {
-        header("Location: index.php");
-        die("Redirecting to index.php"); 
+?>
+        <html>
+            <head>
+                <title> concept browser </title>
+                <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.css" />
+                <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+            </head>
+            <body>
+
+<?php
+
+            $files = scandir('upload');
+            sort($files);                         // this does the sorting
+?>
+                <div style="display:inline;">
+
+<?php
+                foreach($files as $file) {
+                    if (($file != ".") && ($file != "..") && ($file != "private") ) {
+?>
+                <div style="display:inline-block;min-width:250px;overflow:hidden;">
+                    <?= $file; ?>
+                </div>
+
+                <div style="display:inline-block;min-width:250px;overflow:hidden;">
+                    <audio controls>
+                      <source src="http://rogerlsmith.net/concept/upload/<?= $file; ?>" type="audio/mpeg" >
+                    </audio>
+                </div>
+<?php
+            }
+?>
+            </div><div style="clear:both;"></div>
+<?php
+        }
+?>
+            </body>
+        </html>
+<?php
     }
 ?>
