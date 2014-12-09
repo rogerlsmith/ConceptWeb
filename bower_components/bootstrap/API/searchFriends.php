7dc77ecda5a10ae6e74b0329a0ed45f6f1d5dca3
Roger Smith <rogerlsmith@live.com>
2014-12-08 22:54:51 -0500

    cleanup and more work on friends

--- bower_components/bootstrap/API/searchFriends.php
+++ bower_components/bootstrap/API/searchFriends.php
@@ -5,6 +5,8 @@ function searchFriends ( $id ) {
     $path = $_SERVER['DOCUMENT_ROOT'] . "/concept/bower_components/bootstrap/config/config.php";
     require "$path";
     
+    // search contacts email addresses and compate to database
+    
 }
 
 ?>
